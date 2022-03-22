03500e7da4d559357e15becaaffd376fc40f2a5f
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-03-22 23:07:28 +0100

    add functionality for startgame (plays a note) and randomly plays a 2nd note

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,47 @@
-console.log("hi")
+document.getElementById("start-button").addEventListener("click", runGame);
 
 
 
-function runGame(){
-
-}
-
-function playNotes(){
-
-}
-
-function checkAnswer(){
-
-}
-
-function incramentScore(){
+let snare = new Audio('assets/media/snare.mp3')
+let clap = new Audio('assets/media/clap.mp3')
+let sounds = [snare, clap]
+/**
+ * plays base note plus any other note.
+ */
+function runGame() {
+    firstNote()
 
 }
 
-function incramentQuestionNumber(){
+function firstNote() {
+    snare.play()
+    const myTimeout = setTimeout(secondNote, 2000);
+}
+
+function secondNote() {
+    let randomNumber = Math.floor(Math.random() * 10);
+    if (randomNumber >= 5) {
+        snare.play();
+    } else {
+        clap.play();
+    }
+}
+
+
+
+function checkAnswer() {
+
 
 }
 
-function calculateEndScore(){
+function incramentScore() {
 
 }
 
+function incramentQuestionNumber() {
 
+}
 
+function calculateEndScore() {
+
+}
