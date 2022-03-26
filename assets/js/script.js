8b7be555b246169315549291e0390650dcc7e936
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-03-26 17:55:44 +0100

    fixed some issues in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Waits for everything to load and then runs the game
  */
-document.addEventListener("DOMContentLoaded", runGame())
+document.addEventListener("DOMContentLoaded", runGame());
 /**
  * an array of objects containing url's to the samples
  */
@@ -45,7 +45,7 @@ let sounds = [{
         interval: "octave",
         sample: new Audio("assets/media/c2.mp3")
     }
-]
+];
 let interval = "";
 let guess = "";
 /**
@@ -59,7 +59,7 @@ function runGame() {
     Instructions:<br>
     1. The game will play two notes.<br>
     2. Guess the interval.<br>Good Luck<br><p>
-    <br><button aria-label="start game" id="start-game-button">Start Game</button>`
+    <br><button aria-label="start game" id="start-game-button">Start Game</button>`;
     let start = document.getElementById("start-game-button");
     start.addEventListener("click", startGame);
 }
@@ -72,10 +72,10 @@ function startGame() {
     let questionNumber = document.getElementById("question-number-value").innerHTML;
     if (questionNumber < 10) {
         document.getElementById("question-number-value").innerHTML = ++questionNumber;
-        setTimeout(firstNote, 500)
+        setTimeout(firstNote, 500);
 
     } else {
-        scoreSummary()
+        scoreSummary();
     }
 }
 
@@ -83,27 +83,26 @@ function scoreSummary() {
     let score = document.getElementById("right-answers-value").innerHTML;
     let questions = document.getElementById("question-number-value").innerHTML;
     let ratio = parseInt(score) / parseInt(questions);
-    console.log(ratio)
     if (ratio < 0.5) {
         document.getElementById("modal-container").style.display = "flex";
         document.getElementById("modal-content").innerHTML = `<h2 aria-label="your score">You Scored<br>${Math.floor((score)/(questions)*100)}%</h2><br><p>Starting out is never easy. Try again</p><br>
-        <button id="try-again" aria-label="try again button">Try Again</button>`
-        let tryAgain = document.getElementById("try-again")
-        tryAgain.addEventListener("click", reset)
+        <button id="try-again" aria-label="try again button">Try Again</button>`;
+        let tryAgain = document.getElementById("try-again");
+        tryAgain.addEventListener("click", reset);
     }
     if (ratio > 0.5 && ratio < 0.8) {
         document.getElementById("modal-container").style.display = "flex";
         document.getElementById("modal-content").innerHTML = `<h2 aria-label="your score">You Scored<br>${Math.floor((score)/(questions)*100)}%</h2><br><p>Pretty decent!! you're gettin' good ;). Try again</p><br>
-        <button id="try-again" aria-label="try again">Try Again</button>`
-        let tryAgain = document.getElementById("try-again")
-        tryAgain.addEventListener("click", reset)
+        <button id="try-again" aria-label="try again">Try Again</button>`;
+        let tryAgain = document.getElementById("try-again");
+        tryAgain.addEventListener("click", reset);
     }
     if (ratio > 0.8) {
         document.getElementById("modal-container").style.display = "flex";
         document.getElementById("modal-content").innerHTML = `<h2 aria-label="your score">You Scored<br>${Math.floor((score)/(questions)*100)}%</h2><br><br>Great Job!! You are really goooood =)<br>
-        <button id="try-again" aria-label="try again">Try Again</button>`
-        let tryAgain = document.getElementById("try-again")
-        tryAgain.addEventListener("click", reset)
+        <button id="try-again" aria-label="try again">Try Again</button>`;
+        let tryAgain = document.getElementById("try-again");
+        tryAgain.addEventListener("click", reset);
     }
 }
 /**
@@ -112,8 +111,8 @@ function scoreSummary() {
 function firstNote() {
     let audio = sounds[0].sample;
     audio.play();
-    document.getElementById("sound-one").style.animationPlayState = "running"
-    setTimeout(secondNote, 1700)
+    document.getElementById("sound-one").style.animationPlayState = "running";
+    setTimeout(secondNote, 1700);
 }
 /**
  * plays the 2nd note (random)
@@ -121,22 +120,22 @@ function firstNote() {
 function secondNote() {
     let randomNumber = Math.floor(Math.random() * 7);
     let audio = sounds[randomNumber].sample;
-    document.getElementById("sound-one").style.animationPlayState = "paused"
-    document.getElementById("sound-two").style.animationPlayState = "running"
+    document.getElementById("sound-one").style.animationPlayState = "paused";
+    document.getElementById("sound-two").style.animationPlayState = "running";
     audio.play();
     interval = randomNumber;
-    console.log(randomNumber)
-    setTimeout(answer, 500)
+    console.log(randomNumber);
+    setTimeout(answer, 500);
 }
 /**
  * adds eventlisteners to each button and changes the value of guess to the datavalue of the button clicked
  */
 
 function answer() {
-    document.getElementById("sound-two").style.animationPlayState = "paused"
+    document.getElementById("sound-two").style.animationPlayState = "paused";
     let answerButtons = document.getElementsByClassName("answer-buttons");
-    for (button of answerButtons) {
-        button.addEventListener("click", storeAnswer)
+    for (let button of answerButtons) {
+        button.addEventListener("click", storeAnswer);
     }
 }
 /**
@@ -144,40 +143,40 @@ function answer() {
  */
 
 function storeAnswer(event) {
-    guess = event.target.dataset.answer
-    checkAnswer()
+    guess = event.target.dataset.answer;
+    checkAnswer();
 }
 /**
  * checks if interval and guess are the same and increase score wrong/right
  */
+
+ function stopAnimation() {
+    let hearts = document.getElementsByClassName("hearts");
+    for (var heart of hearts) {
+        heart.style.animationPlayState = "paused";
+    }
+}
+
 function checkAnswer() {
 
     if (interval == guess) {
-        console.log("right Answer")
         let rightAnswers = document.getElementById("right-answers-value").innerText;
-        document.getElementById("right-answers-value").innerText = ++rightAnswers
+        document.getElementById("right-answers-value").innerText = ++rightAnswers;
         let hearts = document.getElementsByClassName("hearts");
-        for (heart of hearts) {
-            heart.style.animationPlayState = "running";    
+        for (var heart of hearts) {
+            heart.style.animationPlayState = "running";
+            setTimeout(stopAnimation, 690);  
         }
-        setTimeout(stopAnimation, 690)
-        function stopAnimation(){
-            for (heart of hearts) {
-                heart.style.animationPlayState = "paused";   
-        }
-    }
-    startGame()
-
+        startGame();
     } else {
-
         document.getElementById("modal-container").style.display = "flex";
         let correctAnswer = sounds[interval].interval;
         let userAnswer = sounds[guess].interval;
         let wrongAnswers = document.getElementById("wrong-answers-value").innerText;
-        document.getElementById("wrong-answers-value").innerText = ++wrongAnswers
+        document.getElementById("wrong-answers-value").innerText = ++wrongAnswers;
         document.getElementById("modal-content").innerHTML = `<h2 aria-label="wrong answer">Ooops!</h2> You answered ${userAnswer}. The correct answer was ${correctAnswer}.<br><br><button aria-label=" ok or continue" id="ok">OK!</button>`;
         let okButton = document.getElementById("ok");
-        okButton.addEventListener("click", startGame)
+        okButton.addEventListener("click", startGame);
 
     }
 
@@ -186,18 +185,17 @@ function checkAnswer() {
  * reset values
  */
 function reset() {
-    console.log("reset")
     document.getElementById("question-number-value").innerHTML = 0;
     document.getElementById("right-answers-value").innerHTML = 0;
     document.getElementById("wrong-answers-value").innerHTML = 0;
-    runGame()
+    runGame();
 }
 /**
  * Event Listeners
  */
 let start = document.getElementById("start-game-button");
 start.addEventListener("click", startGame);
-let restartGame = document.getElementById("restart-game-button")
+let restartGame = document.getElementById("restart-game-button");
 restartGame.addEventListener("click", reset);
 
 
